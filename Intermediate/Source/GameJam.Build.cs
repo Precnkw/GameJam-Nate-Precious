634b7a33f99d1cae45156a412a16247ce493e820
Nate <nategallatin@yahoo.com>
2023-07-14 12:21:41 -0400

    time

---
+++ Intermediate/Source/GameJam.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class GameJam : ModuleRules
+{
+	public GameJam(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
